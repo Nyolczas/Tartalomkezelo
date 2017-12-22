c369e210a9ef2094203ad456aa891b9b7ac702c8
Nyolczas <nyolczas@gmail.com>
2017-12-23 00:16:54 +0100

    index

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require_once("adatbazis.php");
+
 /* Menü összeállítása */
 $sql = "SELECT id, alias, menunev
         FROM cms_tartalom
@@ -14,20 +15,20 @@ $menu = '<ul>
          </ul>';
 
 /* Tartalom összeállítása */
-
 $sql = "SELECT menunev, tartalom, modositas, leiras, kulcsszavak
         FROM cms_tartalom
         WHERE id = 1
         LIMIT 1";
 
 $leiras = $sor['leiras'];
-$kulcsszavak = $sor['kulcsszavak'];;
-$menunev = "Bemutatkozás";
-$tartalom = "
-<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>";
+$kulcsszavak = $sor['kulcsszavak'];
+$menunev = $sor['menunev'];
+$tartalom = $sor['tartalom'];
 
+/* Modulok kezelése */
 $oldalsav = "";
 
+//Sablonozó
 $sablon = file_get_contents("sablon.html");
 $sablon = str_replace("{{menunev}}",  $menunev,   $sablon);
 $sablon = str_replace("{{leiras}}",   $leiras,   $sablon);
