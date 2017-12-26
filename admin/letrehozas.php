5b97e7cd65a9be48dedb932628cef0906a00c07f
Nyolczas <nyolczas@gmail.com>
2017-12-26 12:47:46 +0100

    létrehozás

---
+++ admin/letrehozas.php
@@ -0,0 +1,19 @@
+<form method="post" action="">
+	<h3>Új oldal létrehozása</h3>
+	<p><label for="alias">Alias:</label><br>
+	<input type="text" id="alias" name="alias"></p>
+	<p><label for="sorrend">Sorrend:</label><br>
+	<input type="number" id="sorrend" name="sorrend" min="1"></p>
+	<p><label for="menunev">Menunév:*</label><br>
+	<input type="text" id="menunev" name="menunev" required></p>
+	<p><label for="tartalom">Tartalom:</label><br>
+	<textarea id="tartalom" name="tartalom"></textarea></p>
+	<p><label for="leiras">Leírás:</label><br>
+	<textarea id="leiras" name="leiras"></textarea></p>
+	<p><label for="kulcsszavak">Kulcsszavak:</label><br>
+	<textarea id="kulcsszavak" name="kulcsszavak"></textarea></p>
+	<p><label for="statusz">Státusz:</label><br>
+	<select id="statusz" name="statusz">
+	
+	</select>
+</form>
--- admin/szerkesztes.php
+++ admin/szerkesztes.php
@@ -1,12 +1,12 @@
 <?php
 require_once("../adatbazis.php");
-$sql = "SELECT id, alias, sorrend, menunev, tartalom, modositas, statusz
+$sql = "SELECT id, alias, sorrend, menunev, tartalom, statusz
         FROM cms_tartalom
         ORDER BY sorrend ASC";
 
 $eredmeny = mysqli_query($dbconn, $sql);
 
-$tablazat = "<p><a href=\"letrehozas.php\">Új oldal létrehozása</a></p>
+$tablazat = "<p><a href=\"letrehozas.php\"><i class=\"material-icons\">add_box</i> Új oldal készítése</a></p>
 <table>
 <tr>
 	<th>Alias</th>
@@ -14,6 +14,7 @@ $tablazat = "<p><a href=\"letrehozas.php\">Új oldal létrehozása</a></p>
 	<th>Menunev</th>
 	<th>Tartalom</th>
 	<th>Statusz</th>
+	<th>Művelet</th>
 </tr>\n";
 while ($sor = mysqli_fetch_assoc($eredmeny)) {
 	$tablazat.= "<tr>
@@ -21,11 +22,13 @@ while ($sor = mysqli_fetch_assoc($eredmeny)) {
 		<td>{$sor['sorrend']}</td>
 		<td>{$sor['menunev']}</td>
 		<td>".substr(strip_tags($sor['tartalom']), 0, 100)."</td>
-		<td>{$sor['modositas']}</td>
+		<td>{$sor['statusz']}</td>
+		<td><a href=\"modositas.php?id={$sor['id']}\"><i class=\"material-icons\">mode_edit</i></a>
+		<a href=\"torles.php?id={$sor['id']}\"><i class=\"material-icons\">delete_forever</i></a></td>
 	</tr>\n";
 }
 $tablazat.= "</table>
-<p><a href=\"letrehozas.php\">Új oldal létrehozása</a></p>\n";
+<p><a href=\"letrehozas.php\"><i class=\"material-icons\">add_box</i> Új oldal készítése</a></p>\n";
 
 //Sablonozó
 $sablon = file_get_contents("sablon.html");
